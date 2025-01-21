2c98ce634cf7a0cce7470128abb04bbe3bb77283
Leigh Lawhon <leighlawhon@gmail.com>
2025-01-21 13:58:10 -0500

    fixing script

---
+++ _posts/script.js
@@ -0,0 +1,22 @@
+<script>
+$("#nanogallery2").nanogallery2({
+  thumbnailHeight:  150,
+  thumbnailWidth:   150,
+  itemsBaseURL:     '/assets/images/',
+
+  // ### gallery content ###
+  items: [
+      { src: 'wfe.png', srct: 'wfe.png', title: "Program Select" },
+      { src: 'wfe2.png', srct: 'wfe2.png', title: "Program Selector — Before" },
+      { src: 'allotement dash.png', srct: 'dash.png', title: "Allotment Dashboard" },
+      { src: 'split allotment-wfe.png', srct: 'split allotment-wfe.png', title: "Allotment Split" },
+      { src: 'userdash-leigh-wfe.png', srct: 'userdash-leigh-wfe.png', title: "Employee Onboarding" },
+      { src: 'userdash-wfe.png', srct: 'userdash-wfe.png', , title: "Employee Dashboard" },
+      { src: 'studentdash.png', srct: 'studentdash.png', title: "Employee Dashboard" },
+      { src: 'payments-wfe.png', srct: 'payments-wfe.png', title: "Payments" },
+      { src: 'employerdash-wfe.png', srct: 'employerdash-wfe.png', title: "Employer Dashboard" },
+  ]
+});
+
+
+</script>
